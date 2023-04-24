651be941f8c59a2ee1dc35247c1f23852e9fa1e9
Jørgen Høst <pjz633@alumni.ku.dk>
2023-04-24 11:22:51 +0200

    very important changes to result....

--- tools.py
+++ tools.py
@@ -67,7 +67,7 @@ def get_json(url: str, header: dict):
     return json_
 
 def get_evals_bbr(bbr_df: pd.DataFrame, year):
-    property_vals=[{'unitId': bbr_df['unitId'][i], f'eval_prop_{year}': e['propertyValue'], f'eval_land_{year}': e['landValue'], 'last_changed':e['lastChange'], 'eval_year': e['evaluationYear']} 
+    property_vals=[{'guid': bbr_df['guid'][i], f'eval_prop_{year}': e['propertyValue'], f'eval_land_{year}': e['landValue'], f'eval{year}_last_changed':e['lastChange'], 'eval_year': e['evaluationYear']} 
                 for i, evaluationInfos in enumerate(bbr_df['evaluationInfos'])
                 for e in evaluationInfos if e['evaluationYear'] == year]
     
@@ -77,57 +77,7 @@ def get_evals_bbr(bbr_df: pd.DataFrame, year):
     eval_land= f'eval_land_{year}'
     df[eval_prop]=df[eval_prop].astype('int32')
     df[eval_land]=df[eval_land].astype('int32')
-    df['last_changed']=pd.to_datetime(df['last_changed'])
-    df = df.sort_values(by='last_changed', ascending=False).drop_duplicates(subset='unitId', keep='first')
-    df=df.rename(columns={'unitId': 'guid', 'last_changed': 'eval_last_changed'})
+    df[f'eval{year}_last_changed']=pd.to_datetime(df[f'eval{year}_last_changed'])
+    df = df.sort_values(by=f'eval{year}_last_changed', ascending=False).drop_duplicates(subset='guid', keep='first')
     df = df.reset_index(drop=True)
     return df
-
-def convert_latex_table(latex_str):
-    # Find the beginning and end of the tabular environment
-    begin = latex_str.find("\\begin{tabular}")
-    end = latex_str.find("\\end{tabular}")
-
-    # Extract the contents of the tabular environment
-    tabular_str = latex_str[begin:end]
-
-    # Split the tabular environment into rows
-    rows = tabular_str.split("\\\\")
-    rows = [row.strip() for row in rows]
-
-    # Split the first row into columns
-    cols = rows[1].split("&")
-    cols = [col.strip() for col in cols]
-
-    # Get the length of the longest column name
-    max_len = max([len(col) for col in cols])
-
-    # Reformat the table rows
-    new_rows = []
-    for row in rows[2:]:
-        # Split the row into cells
-        cells = row.split("&")
-        cells = [cell.strip() for cell in cells]
-
-        # Pad the column names with spaces
-        cells[0] = cells[0].ljust(max_len)
-
-        # Add the row to the new table
-        new_row = " & ".join(cells)
-        new_rows.append(new_row)
-
-    # Reformat the table columns
-    new_cols = " & ".join([col.ljust(max_len) for col in cols])
-
-    # Reformat the table header
-    header = "\\begin{table}[H]\n\\centering\n\\begin{tabular}{@{}lcccc@{}}\n\\toprule\n"
-    header += " " * max_len + " & \\multicolumn{3}{c}{Quantile regressions} & \\\\\\cmidrule(lr){2-4}\n"
-    header += new_cols + " & OLS-FE \\\\\n\\midrule\n"
-
-    # Reformat the table footer
-    footer = "\\bottomrule\n\\end{tabular}\n\\end{table}\n"
-
-    # Combine the header, new rows, and footer
-    new_tabular_str = header + "\n".join(new_rows) + "\n" + footer
-
-    return new_tabular_str
